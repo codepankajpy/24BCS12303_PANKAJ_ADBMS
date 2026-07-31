399531726277f2e08c113958cb7e7e9a3e0fc885
code-pankaj <pankajyadavpy069@gmail.com>
2026-07-31 15:12:17 +0530

    updated 4.1 to 3.4

--- experiments/experiment-4/exp4.1.sql
+++
@@ -1,6 +0,0 @@
--- PROBLEM STATEMENT 
--- https://leetcode.com/problems/employee-bonus/
-
--- SOLUTION
-
-SELECT name, bonus FROM Employee as e LEFT JOIN Bonus as b on e.empId=b.empId where bonus<1000 or bonus is NULL;
